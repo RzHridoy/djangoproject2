b6dff1b0f7ba8917d940b78c6363ad788c4f9bf7
Orfiaj Karim <orfiajkarim@gmail.com>
2022-09-15 16:26:32 +0600

    Fix django

--- App_Post/views.py
+++ App_Post/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render
 
 from App_Login.models import UserProfileModel
 from App_Post.models import PostModel
-
+from django.contrib.auth.models import User
 # Create your views here.
 
 
 def home_view(request):
     user_list = UserProfileModel.objects.all()
+    users_list = User.objects.all()
     user_post_list = PostModel.objects.all()
     return render(request, 'App_Post/home.html', context={
         'user_list': user_list,
+        'users_list': users_list,
         'user_post_list': user_post_list
     })
